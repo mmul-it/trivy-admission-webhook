5d13d4b72aa46e7f8a8d8f9d649ce8f10fbd03c1
Raoul Scarazzini <rasca@mmul.it>
2023-07-11 18:47:54 +0200

    Moving to CherryPy for the main app
    Since the Flask application is not suitable for production, this commit
    reimplements the logic by using CherryPy.

--- app.py
+++ app.py
@@ -1,35 +1,35 @@
+import cherrypy
 from subprocess import Popen
-from flask import Flask, request, jsonify
 
-admission_controller = Flask(__name__)
-
-@admission_controller.route("/validate", methods=["POST"])
-def deployment_webhook():
-    request_info = request.get_json()
-    uid = request_info["request"]["uid"]
-    is_secure = True
-    for each_image in request_info["request"]["object"]["spec"]["containers"]:
-        command = [
-            "trivy",
-            "image",
-            "-f",
-            "json",
-            "-s",
-            "CRITICAL",
-            "--exit-code",
-            "1",
-            each_image["image"],
-        ]
-        print("Running command: %s" % " ".join(command))
-        r = Popen(command)
-        r.communicate()
-        if r.returncode == 1:
-            is_secure = False
-
-    if is_secure:
-        return admission_response(True, "All containers are secure", uid)
-    return admission_response(False, "Not all containers secure, failing ...", uid)
-
+class admission_webhook:
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    @cherrypy.tools.json_in()
+    def validate(self, **keywords):
+        request_info = cherrypy.request.json
+        uid = request_info["request"]["uid"]
+        is_secure = True
+        for each_image in request_info["request"]["object"]["spec"]["containers"]:
+            command = [
+                "trivy",
+                "image",
+                "-f",
+                "json",
+                "-s",
+                "CRITICAL",
+                "--exit-code",
+                "1",
+                each_image["image"],
+            ]
+            print("Running command: %s" % " ".join(command))
+            r = Popen(command)
+            r.communicate()
+            if r.returncode == 1:
+                is_secure = False
+    
+        if is_secure:
+            return admission_response(True, "All containers are secure", uid)
+        return admission_response(False, "Not all containers secure, failing ...", uid)
 
 def admission_response(allowed, message, uid):
     msg = {
@@ -37,10 +37,17 @@ def admission_response(allowed, message, uid):
         "kind": "AdmissionReview",
         "response": {"uid": uid, "allowed": allowed, "status": {"message": message}},
     }
-    return jsonify(msg)
-
+    return msg
 
 if __name__ == "__main__":
-    admission_controller.run(
-        host="0.0.0.0", port=443, ssl_context=("/certs/tls.crt", "/certs/tls.key" ), debug=True
-    )
+    server_config={
+        'server.socket_host': '0.0.0.0',
+        'server.socket_port':443,
+ 
+        'server.ssl_module':'pyopenssl',
+        'server.ssl_certificate':'/certs/tls.crt',
+        'server.ssl_private_key':'/certs/tls.key',
+   }
+
+cherrypy.config.update(server_config)
+cherrypy.quickstart(admission_webhook())
